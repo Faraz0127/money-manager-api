56f2eba061ca8de7f02d27aaae04c29d6f5d2d83
Faraz Pasha <farazpasha098@gmail.com>
2026-03-12 23:23:31 +0530

    fix config

--- src/main/java/com/codewithfaraz/moneymanager/config/SecurityConfig.java
+++ src/main/java/com/codewithfaraz/moneymanager/config/SecurityConfig.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -31,21 +31,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-
         httpSecurity
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1.0/**").permitAll()
-                        .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers(
+                                "/status",
+                                "/health",
+                                "/api/v1.0/register",
+                                "/api/v1.0/login",
+                                "/api/v1.0/activate"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -67,10 +71,12 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager() {
+
         DaoAuthenticationProvider provider =
                 new DaoAuthenticationProvider(appUserDetailsService);
-        //provider.setUserDetailsService((org.springframework.security.core.userdetails.UserDetailsService) appUserDetailsService);
+
         provider.setPasswordEncoder(passwordEncoder());
+
         return new ProviderManager(provider);
     }
 
